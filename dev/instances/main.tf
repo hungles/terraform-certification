51b8139bef9cbae04c5cb896d943319d45e6ceec
Sebastian Carmona <scarmonu@emeal.nttdata.com>
2026-07-09 08:23:30 -0500

    Deploy terraform infrastructure

--- dev/instances/main.tf
+++ dev/instances/main.tf
@@ -13,3 +13,11 @@ module "instance_2" {
   subnet_id         = data.aws_subnets.private_with_tag.ids[1]
   ami_id            = data.aws_ami.ubuntu.id
 }
+
+module "instance_3" {
+  source            = "../../modules/ec2"
+  ec2_instance_name = "instancia-3"
+  instance_type     = var.instance_type
+  subnet_id         = data.aws_subnets.private_with_tag.ids[0]
+  ami_id            = data.aws_ami.ubuntu.id
+}
